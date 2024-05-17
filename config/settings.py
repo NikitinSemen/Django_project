7f03ef49a549192c16d3ef3e0b5648e2992c972b
webcam <pr.pypblrka@gmail.com>
2024-05-17 18:06:51 +0400

    домашняя работа 20.2

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -83,7 +84,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIR = (BASE_DIR / 'static',)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
