8c9c0ce2f4c25cea1b00b5af81f0ab80278c2a25
webcam <pr.pypblrka@gmail.com>
2024-05-19 11:23:55 +0400

    собрана страница, выведены товары.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 
-from catalog.views import products_list
+from catalog.views import products_list, product_details
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', products_list, name='products_list')
+    path('', products_list, name='products_list'),
+    path('products/<int:pk>/', product_details, name='product_details')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product, Category
 
@@ -7,6 +7,12 @@ def products_list(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, "products_temp.html", context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_details.html', context)
 
 # # def contacts(request):
 #     if request.method == "POST":
