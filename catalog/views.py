a16b2c099c7e7772941808d900829ddc8fdf0170
webcam <pr.pypblrka@gmail.com>
2024-05-26 23:10:36 +0400

    выполнение домашнего задания

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
 
-from catalog.models import Product
+from catalog.models import Product, BlogPost
 
 
 class ProductListView(ListView):
@@ -36,3 +37,56 @@ class ProductUpdateView(UpdateView):
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy("catalog:products_list")
+
+
+class BlogPostListView(ListView):
+    model = BlogPost
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.first(is_published=True)
+        return queryset
+
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_counter += 1
+        self.object.save()
+        return self.object
+
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    fields = ('title', 'content', 'photo', 'created_at', 'is_published')
+    success_url = reverse_lazy('blogpost:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+            return super().form_valid(form)
+
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    fields = ('title', 'content', 'photo', 'created_at', 'is_published')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+            return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blogpost:view', args=[self.kwargs.get('pk')])
+
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    success_url = reverse_lazy('blogpost:list')
+
